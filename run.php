5024bedb8443f5370104820b3b71763786859ae1
heykha <56066080+heykha@users.noreply.github.com>
2019-11-05 17:17:41 +0700

    Update run.php

--- run.php
+++ run.php
@@ -17,58 +17,22 @@ $pilih = trim(fgets(STDIN));
 echo '####################################';
 echo "\nWait for load data...\n";
 sleep(2);
-echo "total data mahasiswa = 36 \n";
 garis(40);
 echo "\nmasukkan jumlah anggota per kelompok : ";
 $jumlah = trim(fgets(STDIN)); 
 echo "\nprocessing data... \n";
 sleep(3);
 flush();
-$totalkel = 36 / $jumlah;
+$totalkel = 36 / $jumlah; // 36 ganti dengan jumlah data / array kalian
 $tot = $totalkel;
-if(36 % $jumlah == 1 ) {
+if(36 % $jumlah == 1 ) { // 36 ganti juga
 	echo "\n anggota kelompok akan ada yang ganjil, sesuai kesepatakan yang ganjil akan ikut kelompok terakhir\n";
 	}
 	sleep(2);
 garis(30);
 echo "\ntotal kelompok = $tot\n";
 
-$data = array("201853010 AHMAD SOFIAN",
-"201853054 MUHAMMAD ABDUL KHAFID",
-"201853126 SISKA MAY LANI",
-"201853006 MARIO ERVAN ARIYANTO",
-"201853140 TYHAN TASI'UL CHAKIM",
-"201853007 GITALITA PUTRANTI FAHDLI",
-"201853101 ARDIAN KARISFIANTO",
-"201853096 AINUN RIZQIYAA SABIILAA",
-"201853037 Yudi Santoso",
-"201853162 ARDIAN ANGGA PRATAMA",
-"201853087 PUNGKI PUJIYANTO",
-"201853082 KHIRDA LA'ALIYA FIRDAUS",
-"201853168 MUHAMMAD AGUS RIYANTO",
-"201853027 KHOIRUL UMAM",
-"201853080 AGUNG MIFDARYADI",
-"201853088 ABDULLAH IBNU HASAN",
-"201853001 MOHAMMAD ALAIKA FAUZI",
-"201853005 RAHDILLA FAIRUZ SAUFIKA",
-"201853002 DEVI PUSPITA SARI",
-"201853063 AJI SAKTI PUMANDALA",
-"201853158 AJI KURNIAWAN",
-"201853008 ANAS BURHANUDDIN",
-"201853056 INDAH YUNI RETNONINGTYAS",
-"201853070 AGUS RIYANTO",
-"201853020 SYARIF HIDAYAT",
-"201853105 NUR HASAN ANWAS DANIAL",
-"201853067 ABDILLAH KARTIKO",
-"201853048 DWI FEBRI ZULFA",
-"201853103 ALFIN NUR MUBAROK",
-"201853069 WINDA TRIYASNIKA",
-"201853139 Yogi  Prasetyo",
-"201853157 ADI SULISTIYONO",
-"201853161 MUHAMMAD ARIF FAISAL",
-"201853055 BRENDAN ANDIKA PRANANDA",
-"201853024 KUSDIYANA NURUL MURDIYATI",
-"201853013 MUHAMMAD ADIB IQBAL HAQ");
+$data = array("nama mahasiswa 1", "nama mahasiswa 2", "nama mahasiswa 3"); // sesuaikan dengan array kalian ( nama mahasiwa2 dikelas)
 		
 for ($i = 0; $i<$tot; $i++){
 	garis(30);
@@ -83,23 +47,3 @@ echo "\n$ii > $hasil\n";
 }
 }
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
